f097da93fdfc4011f21ae50e0cebafab349dd129
IsumiKalpage <isumikalpage@gmail.com>
2025-06-08 17:20:55 +0530

    did some changes in the login.dart page

--- lib/screens/login.dart
+++ lib/screens/login.dart
@@ -1,9 +1,7 @@
 import 'package:flutter/material.dart';
 import 'package:mad_veloura/models/components/bottomnavbar.dart';
-import 'register.dart'; 
-// ignore: unused_import
-import 'package:mad_veloura/screens/home.dart'; 
-import '../models/user/user_list.dart'; 
+import 'register.dart';
+import '../models/user/user_list.dart';
 
 class LoginScreen extends StatefulWidget {
   const LoginScreen({super.key});
@@ -28,7 +26,7 @@ class _LoginScreenState extends State<LoginScreen> {
       // If valid, navigate to BottomNavbar 
       Navigator.pushReplacement(
         context,
-        MaterialPageRoute(builder: (context) => const BottomNavbar()), // Navigate to BottomNavbar
+        MaterialPageRoute(builder: (context) => const BottomNavbar()), 
       );
     } else {
       // If invalid, show an error message
@@ -48,80 +46,117 @@ class _LoginScreenState extends State<LoginScreen> {
             padding: const EdgeInsets.symmetric(horizontal: 20.0),
             child: Column(
               mainAxisAlignment: MainAxisAlignment.center,
+              crossAxisAlignment: CrossAxisAlignment.center,
               children: [
-                // Logo image
+                
                 Image.asset(
                   'assets/images/logo.png',
-                  height: 150,
-                  width: 150,
+                  height: 180, 
+                  width: 180,
                 ),
+                SizedBox(height: 20), 
 
-                // Login Text
-                SizedBox(height: 20),
-                Text(
-                  'Login',
-                  style: TextStyle(
-                    color: Colors.black,
-                    fontSize: 32,
-                    fontWeight: FontWeight.w600,
+                
+                Container(
+                  decoration: BoxDecoration(
+                    boxShadow: [
+                      BoxShadow(
+                        color: Colors.grey.withOpacity(0.3),
+                        blurRadius: 10,
+                        spreadRadius: 5,
+                        offset: Offset(0, 4),
+                      ),
+                    ],
+                    color: Colors.white,
+                  ),
+                  padding: EdgeInsets.symmetric(horizontal: 20, vertical: 40), 
+                  width: double.infinity,
+                  child: Column(
+                    children: [
+                      
+                      Text(
+                        'Login',
+                        style: TextStyle(
+                          color: Colors.black,
+                          fontSize: 32,
+                          fontWeight: FontWeight.w300, 
+                          fontFamily: 'Roboto', 
+                        ),
+                      ),
+                      SizedBox(height: 30), 
+                      // Email input field with icon
+                      Container(
+                        decoration: BoxDecoration(
+                          color: Colors.grey[100],
+                        ),
+                        child: TextField(
+                          controller: emailController,
+                          decoration: InputDecoration(
+                            prefixIcon: Icon(Icons.email),
+                            hintText: 'Enter your email',
+                            border: InputBorder.none,
+                            contentPadding: EdgeInsets.symmetric(vertical: 15.0, horizontal: 20.0),
+                          ),
+                        ),
+                      ),
+                      SizedBox(height: 20), 
+
+                      // Password input field with icon
+                      Container(
+                        decoration: BoxDecoration(
+                          color: Colors.grey[100],
+                        ),
+                        child: TextField(
+                          controller: passwordController,
+                          obscureText: true,
+                          decoration: InputDecoration(
+                            prefixIcon: Icon(Icons.lock),
+                            hintText: 'Enter your password',
+                            border: InputBorder.none,
+                            contentPadding: EdgeInsets.symmetric(vertical: 15.0, horizontal: 20.0),
+                          ),
+                        ),
+                      ),
+                      SizedBox(height: 20), 
+                      // Forgot Password link
+                      GestureDetector(
+                        onTap: () {
+                          
+                        },
+                        child: Text(
+                          "Forgot Password?",
+                          style: TextStyle(
+                            color: const Color.fromARGB(255, 164, 161, 162),
+                            fontSize: 16,
+                            decoration: TextDecoration.underline,
+                          ),
+                        ),
+                      ),
+                      SizedBox(height: 30),
+
+                      // Login Button
+                      ElevatedButton(
+                        onPressed: _login,
+                        style: ElevatedButton.styleFrom(
+                          backgroundColor: Color(0xFF823434), 
+                          padding: EdgeInsets.symmetric(horizontal: 60, vertical: 16),
+                          shape: RoundedRectangleBorder(
+                            borderRadius: BorderRadius.circular(0), 
+                          ),
+                          elevation: 8,
+                          minimumSize: Size(double.infinity, 55),
+                        ),
+                        child: Text(
+                          'Login',
+                          style: TextStyle(
+                            color: Colors.white,
+                            fontSize: 18,
+                          ),
+                        ),
+                      ),
+                    ],
                   ),
                 ),
-
-                // Email input field
-                SizedBox(height: 30),
-                TextField(
-                  controller: emailController,
-                  decoration: InputDecoration(
-                    labelText: 'Email',
-                    labelStyle: TextStyle(color: Colors.black),
-                    filled: true,
-                    fillColor: Colors.grey[200],
-                    border: OutlineInputBorder(
-                      borderRadius: BorderRadius.circular(12.0),
-                    ),
-                    contentPadding: EdgeInsets.symmetric(vertical: 15.0, horizontal: 20.0),
-                  ),
-                ),
-
-                // Password input field
-                SizedBox(height: 20),
-                TextField(
-                  controller: passwordController,
-                  obscureText: true,
-                  decoration: InputDecoration(
-                    labelText: 'Password',
-                    labelStyle: TextStyle(color: Colors.black),
-                    filled: true,
-                    fillColor: Colors.grey[200],
-                    border: OutlineInputBorder(
-                      borderRadius: BorderRadius.circular(12.0),
-                    ),
-                    contentPadding: EdgeInsets.symmetric(vertical: 15.0, horizontal: 20.0),
-                  ),
-                ),
-
-                // Login button
-                SizedBox(height: 30),
-                ElevatedButton(
-                  onPressed: _login,
-                  style: ElevatedButton.styleFrom(
-                    backgroundColor: Color(0xFF823434), // Button color set to #823434
-                    padding: EdgeInsets.symmetric(horizontal: 80, vertical: 16),
-                    shape: RoundedRectangleBorder(
-                      borderRadius: BorderRadius.circular(12),
-                    ),
-                    elevation: 5,
-                  ),
-                  child: Text(
-                    'Login',
-                    style: TextStyle(
-                      color: Colors.white,
-                      fontSize: 18,
-                    ),
-                  ),
-                ),
-
-                // Sign-up link
                 SizedBox(height: 20),
                 GestureDetector(
                   onTap: () {
@@ -139,6 +174,29 @@ class _LoginScreenState extends State<LoginScreen> {
                       decoration: TextDecoration.underline,
                     ),
                   ),
+                ),
+                SizedBox(height: 30),
+
+                // Social Media Login 
+                Row(
+                  mainAxisAlignment: MainAxisAlignment.center,
+                  children: [
+                    IconButton(
+                      icon: Icon(Icons.g_translate),
+                      onPressed: () {},
+                      color: const Color.fromARGB(255, 113, 203, 116),
+                    ),
+                    IconButton(
+                      icon: Icon(Icons.facebook),
+                      onPressed: () {},
+                      color: const Color.fromARGB(255, 6, 65, 192),
+                    ),
+                    IconButton(
+                      icon: Icon(Icons.camera_alt), // Instagram icon
+                      onPressed: () {},
+                      color: const Color.fromARGB(255, 213, 68, 117),
+                    ),
+                  ],
                 ),
               ],
             ),
--- lib/screens/products.dart
+++ lib/screens/products.dart
@@ -53,6 +53,28 @@ class ProductsScreen extends StatelessWidget {
                     name: _getSkinCareProductName(index),
                     price: _getSkinCareProductPrice(index),
                     rating: _getSkinCareProductRating(index),
+                    onTap: () {
+                      if (index == 0) {
+                        Navigator.push(
+                          context,
+                          MaterialPageRoute(
+                            builder: (context) => ProductDetailPage(
+                              imageUrl: _getSkinCareImageUrl(index),
+                              name: _getSkinCareProductName(index),
+                              price: _getSkinCareProductPrice(index),
+                              rating: _getSkinCareProductRating(index),
+                              description: 'The AHA 30% + BHA 2% Peeling Solution is an at-home exfoliating facial that combines the power of alpha-hydroxy acids (AHA) and beta-hydroxy acids (BHA) to improve skin texture, reduce pore congestion, and address uneven skin tone. This high-strength peel is a recommended facial exfoliator for experienced users looking to achieve smoother, clearer skin. It features a Tasmanian pepperberry derivative, a known anti-irritant and is further supported with hyaluronic acid, pro-vitamin B5 and black carrot.',
+                              features: 'Targets - Textural Irregularities, Dullness, Uneven Skin Tone',
+                              brand: 'The Ordinary',
+                              deliveryCharge: 'LKR 350',
+                              authenticity: 'Guaranteed 100% Authentic Products',
+                              origin: 'Imported from South Korea',
+                              securePayments: 'Secure payments',
+                            ),
+                          ),
+                        );
+                      }
+                    },
                   );
                 },
               ),
@@ -86,6 +108,28 @@ class ProductsScreen extends StatelessWidget {
                     name: _getCosmeticsProductName(index),
                     price: _getCosmeticsProductPrice(index),
                     rating: _getCosmeticsProductRating(index),
+                    onTap: () {
+                      if (index == 0) {
+                        Navigator.push(
+                          context,
+                          MaterialPageRoute(
+                            builder: (context) => ProductDetailPage(
+                              imageUrl: _getCosmeticsImageUrl(index),
+                              name: _getCosmeticsProductName(index),
+                              price: _getCosmeticsProductPrice(index),
+                              rating: _getCosmeticsProductRating(index),
+                              description: 'Glass-like gloss filled with moisture! Moisture Glow Lip Gloss',
+                              features: 'Makes your lips glossy,Maintain vivid colors,Makes your lips glossy and fluffy',
+                              brand: 'Hemish',
+                              deliveryCharge: 'LKR 350',
+                              authenticity: 'Guaranteed 100% Authentic Products',
+                              origin: 'Imported from South Korea',
+                              securePayments: 'Secure payments',
+                            ),
+                          ),
+                        );
+                      }
+                    },
                   );
                 },
               ),
@@ -119,6 +163,28 @@ class ProductsScreen extends StatelessWidget {
                     name: _getHairBodyProductName(index),
                     price: _getHairBodyProductPrice(index),
                     rating: _getHairBodyProductRating(index),
+                    onTap: () {
+                      if (index == 0) {
+                        Navigator.push(
+                          context,
+                          MaterialPageRoute(
+                            builder: (context) => ProductDetailPage(
+                              imageUrl: _getHairBodyImageUrl(index),
+                              name: _getHairBodyProductName(index),
+                              price: _getHairBodyProductPrice(index),
+                              rating: _getHairBodyProductRating(index),
+                              description: 'Nourish and cleanse dry, weak, or brittle hair with this scalp-tingling hair strengthening shampoo! Mielles Rosemary Mint Strengthening Shampoo was developed to gently cleanse your hair while providing key nutrients. With hair strengthening biotin and certified organic ingredients such as coconut and babassu seed oil, you can bring weak or brittle hair back from the brink.',
+                              features: 'Infused with biotin to encourage growth, Gently cleanses and protects hair, Smells great!',
+                              brand: 'Brand Name',
+                              deliveryCharge: 'LKR 350',
+                              authenticity: 'Guaranteed 100% Authentic Products',
+                              origin: 'Imported from South Korea',
+                              securePayments: 'Secure payments',
+                            ),
+                          ),
+                        );
+                      }
+                    },
                   );
                 },
               ),
@@ -258,79 +324,188 @@ class ProductCard extends StatelessWidget {
   final String name;
   final String price;
   final double rating;
+  final VoidCallback onTap;
 
   const ProductCard({
     required this.imageUrl,
     required this.name,
     required this.price,
     required this.rating,
+    required this.onTap,
   });
 
   @override
   Widget build(BuildContext context) {
-    return Card(
-      elevation: 4.0,
-      shape: RoundedRectangleBorder(
-        borderRadius: BorderRadius.circular(12.0),
-      ),
-      child: Column(
-        children: [
-          Image.asset(
-            imageUrl,
-            width: double.infinity, 
-            height: 200, 
-            fit: BoxFit.cover,
-          ),
-          Padding(
-            padding: const EdgeInsets.all(12.0),
-            child: Column(
-              crossAxisAlignment: CrossAxisAlignment.start,
-              children: [
-                Text(
-                  name,
-                  style: TextStyle(
-                    fontSize: 14,
-                    fontWeight: FontWeight.w200,
-                    color: Colors.black,
+    return GestureDetector(
+      onTap: onTap,
+      child: Card(
+        elevation: 4.0,
+        shape: RoundedRectangleBorder(
+          borderRadius: BorderRadius.circular(12.0),
+        ),
+        child: Column(
+          children: [
+            Image.asset(
+              imageUrl,
+              width: double.infinity, 
+              height: 200, 
+              fit: BoxFit.cover,
+            ),
+            Padding(
+              padding: const EdgeInsets.all(12.0),
+              child: Column(
+                crossAxisAlignment: CrossAxisAlignment.start,
+                children: [
+                  Text(
+                    name,
+                    style: TextStyle(
+                      fontSize: 14,
+                      fontWeight: FontWeight.w200,
+                      color: Colors.black,
+                    ),
+                    maxLines: 2,
+                    overflow: TextOverflow.ellipsis,
                   ),
-                  maxLines: 2,
-                  overflow: TextOverflow.ellipsis,
-                ),
-                SizedBox(height: 8.5),
-                Text(
-                  price,
-                  style: TextStyle(
-                    fontSize: 12,
-                    fontWeight: FontWeight.w400,
-                    color: Colors.grey,
-                  ),
-                ),
-                SizedBox(height: 9.0),
-                Row(
-                  children: List.generate(
-                    5,
-                    (index) => Icon(
-                      index < rating ? Icons.star : Icons.star_border,
-                      color: Colors.amber,
-                      size: 17.0,
+                  SizedBox(height: 8.5),
+                  Text(
+                    price,
+                    style: TextStyle(
+                      fontSize: 12,
+                      fontWeight: FontWeight.w400,
+                      color: Colors.grey,
                     ),
                   ),
-                ),
-                SizedBox(height: 8.0),
-                ElevatedButton(
-                  onPressed: () {},
-                  child: Text('Add to Cart'),
-                  style: ElevatedButton.styleFrom(
-                    foregroundColor: Colors.white,
-                    backgroundColor: Color.fromARGB(255, 169, 121, 121),
-                    minimumSize: Size(double.infinity, 36),
-                    padding: EdgeInsets.symmetric(vertical: 8.0),
+                  SizedBox(height: 9.0),
+                  Row(
+                    children: List.generate(
+                      5,
+                      (index) => Icon(
+                        index < rating ? Icons.star : Icons.star_border,
+                        color: Colors.amber,
+                        size: 17.0,
+                      ),
+                    ),
+                  ),
+                  SizedBox(height: 8.0),
+                  ElevatedButton(
+                    onPressed: () {},
+                    child: Text('Add to Cart'),
+                    style: ElevatedButton.styleFrom(
+                      foregroundColor: Colors.white,
+                      backgroundColor: Color.fromARGB(255, 169, 121, 121),
+                      minimumSize: Size(double.infinity, 36),
+                      padding: EdgeInsets.symmetric(vertical: 8.0),
+                    ),
+                  ),
+                ],
+              ),
+            ),
+          ],
+        ),
+      ),
+    );
+  }
+}
+
+class ProductDetailPage extends StatelessWidget {
+  final String imageUrl;
+  final String name;
+  final String price;
+  final double rating;
+  final String description;
+  final String features;
+  final String brand;
+  final String deliveryCharge;
+  final String authenticity;
+  final String origin;
+  final String securePayments;
+
+  const ProductDetailPage({
+    required this.imageUrl,
+    required this.name,
+    required this.price,
+    required this.rating,
+    required this.description,
+    required this.features,
+    required this.brand,
+    required this.deliveryCharge,
+    required this.authenticity,
+    required this.origin,
+    required this.securePayments,
+  });
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(title: Text(name)),
+      body: SingleChildScrollView(
+        child: Padding(
+          padding: const EdgeInsets.all(16.0),
+          child: Column(
+            crossAxisAlignment: CrossAxisAlignment.start,
+            children: [
+              Center(
+                child: Image.asset(imageUrl, width: 250, height: 250, fit: BoxFit.cover),
+              ),
+              SizedBox(height: 16),
+              Text(
+                name,
+                style: TextStyle(fontSize: 24, fontWeight: FontWeight.bold),
+              ),
+              SizedBox(height: 8),
+              Text(
+                'Brand: $brand',
+                style: TextStyle(fontSize: 16, fontWeight: FontWeight.w400, decoration: TextDecoration.underline),
+              ),
+              SizedBox(height: 8),
+              Row(
+                children: List.generate(
+                  5,
+                  (index) => Icon(
+                    index < rating ? Icons.star : Icons.star_border,
+                    color: Colors.amber,
+                    size: 17.0,
                   ),
                 ),
-              ],
-            ),
+              ),
+              SizedBox(height: 16),
+              Text(
+                'Description:',
+                style: TextStyle(fontSize: 18, fontWeight: FontWeight.bold),
+              ),
+              SizedBox(height: 8),
+              Text(description),
+              SizedBox(height: 16),
+              Text(
+                'Features:',
+                style: TextStyle(fontSize: 18, fontWeight: FontWeight.bold),
+              ),
+              SizedBox(height: 8),
+              Text(features),
+              SizedBox(height: 16),
+              Text(
+                'Delivery Features:',
+                style: TextStyle(fontSize: 18, fontWeight: FontWeight.bold),
+              ),
+              SizedBox(height: 8),
+              Text('Delivery Charge: $deliveryCharge'),
+              Text('Authenticity: $authenticity'),
+              Text('Origin: $origin'),
+              Text('Secure Payments: $securePayments'),
+              SizedBox(height: 16),
+              ElevatedButton(
+                onPressed: () {},
+                child: Text('Add to Cart'),
+                style: ElevatedButton.styleFrom(
+                  foregroundColor: Colors.white,
+                  backgroundColor: Color.fromARGB(255, 169, 121, 121),
+                  minimumSize: Size(double.infinity, 36),
+                  padding: EdgeInsets.symmetric(vertical: 8.0),
+                ),
+              ),
+            ],
           ),
-        ],
+        ),
       ),
     );
   }
