6cc3a4bb6724717fb09676dcb79c9643cb6ec834
IsumiKalpage <isumikalpage@gmail.com>
2025-06-08 16:09:42 +0530

    did some changes in profile.dart

--- lib/screens/profile.dart
+++ lib/screens/profile.dart
@@ -75,11 +75,11 @@ class ProfileScreen extends StatelessWidget {
           children: const [
             Text(
               'User Name', 
-              style: TextStyle(fontSize: 22, fontWeight: FontWeight.bold),
+              style: TextStyle(fontSize: 20, fontWeight: FontWeight.bold),
             ),
             Text(
               'user@example.com', 
-              style: TextStyle(fontSize: 16, color: Colors.grey),
+              style: TextStyle(fontSize: 14, color: Colors.grey),
             ),
           ],
         ),
@@ -94,7 +94,7 @@ class ProfileScreen extends StatelessWidget {
       children: [
         const Text(
           'Preferences',
-          style: TextStyle(fontSize: 18, fontWeight: FontWeight.bold),
+          style: TextStyle(fontSize: 16, fontWeight: FontWeight.bold),
         ),
         const SizedBox(height: 10),
         ListTile(
@@ -102,9 +102,8 @@ class ProfileScreen extends StatelessWidget {
           title: const Text('Dark Mode'),
           trailing: Switch(
             value: true, 
-            onChanged: (bool value) {
-
-            },
+            onChanged: (bool value) {},
+            activeColor: Color.fromARGB(255, 169, 121, 121),
           ),
         ),
       ],
@@ -118,24 +117,20 @@ class ProfileScreen extends StatelessWidget {
       children: [
         const Text(
           'Account Management',
-          style: TextStyle(fontSize: 18, fontWeight: FontWeight.bold),
+          style: TextStyle(fontSize: 16, fontWeight: FontWeight.bold),
         ),
         const SizedBox(height: 10),
         ListTile(
           leading: const Icon(Icons.account_circle),
           title: const Text('Account Information'),
           trailing: const Icon(Icons.arrow_forward),
-          onTap: () {
-            
-          },
+          onTap: () {},
         ),
         ListTile(
           leading: const Icon(Icons.credit_card),
           title: const Text('Billing'),
           trailing: const Icon(Icons.arrow_forward),
-          onTap: () {
-            
-          },
+          onTap: () {},
         ),
       ],
     );
@@ -148,24 +143,20 @@ class ProfileScreen extends StatelessWidget {
       children: [
         const Text(
           'More Information',
-          style: TextStyle(fontSize: 18, fontWeight: FontWeight.bold),
+          style: TextStyle(fontSize: 16, fontWeight: FontWeight.bold),
         ),
         const SizedBox(height: 10),
         ListTile(
           leading: const Icon(Icons.info),
           title: const Text('About Us'),
           trailing: const Icon(Icons.arrow_forward),
-          onTap: () {
-            
-          },
+          onTap: () {},
         ),
         ListTile(
           leading: const Icon(Icons.security),
           title: const Text('Privacy'),
           trailing: const Icon(Icons.arrow_forward),
-          onTap: () {
-            
-          },
+          onTap: () {},
         ),
       ],
     );
@@ -178,15 +169,13 @@ class ProfileScreen extends StatelessWidget {
       children: [
         const Text(
           'Account Actions',
-          style: TextStyle(fontSize: 18, fontWeight: FontWeight.bold),
+          style: TextStyle(fontSize: 16, fontWeight: FontWeight.bold),
         ),
         const SizedBox(height: 10),
         ListTile(
           leading: const Icon(Icons.exit_to_app),
           title: const Text('Logout'),
-          onTap: () {
-            
-          },
+          onTap: () {},
         ),
       ],
     );
@@ -199,7 +188,7 @@ class ProfileScreen extends StatelessWidget {
       children: [
         const Text(
           'Contact Information',
-          style: TextStyle(fontSize: 18, fontWeight: FontWeight.bold),
+          style: TextStyle(fontSize: 16, fontWeight: FontWeight.bold),
         ),
         const SizedBox(height: 10),
         ListTile(
@@ -207,18 +196,14 @@ class ProfileScreen extends StatelessWidget {
           title: const Text('Phone Number'),
           subtitle: const Text('+1234567890'),
           trailing: const Icon(Icons.edit),
-          onTap: () {
-            
-          },
+          onTap: () {},
         ),
         ListTile(
           leading: const Icon(Icons.email),
           title: const Text('Email Address'),
           subtitle: const Text('user@example.com'),
           trailing: const Icon(Icons.edit),
-          onTap: () {
-            
-          },
+          onTap: () {},
         ),
       ],
     );
@@ -231,24 +216,20 @@ class ProfileScreen extends StatelessWidget {
       children: [
         const Text(
           'Security Settings',
-          style: TextStyle(fontSize: 18, fontWeight: FontWeight.bold),
+          style: TextStyle(fontSize: 16, fontWeight: FontWeight.bold),
         ),
         const SizedBox(height: 10),
         ListTile(
           leading: const Icon(Icons.lock),
           title: const Text('Change Password'),
           trailing: const Icon(Icons.arrow_forward),
-          onTap: () {
-           
-          },
+          onTap: () {},
         ),
         ListTile(
           leading: const Icon(Icons.fingerprint),
           title: const Text('Enable Fingerprint'),
           trailing: const Icon(Icons.check_box_outline_blank),
-          onTap: () {
-            
-          },
+          onTap: () {},
         ),
       ],
     );
@@ -261,7 +242,7 @@ class ProfileScreen extends StatelessWidget {
       children: [
         const Text(
           'Notification Preferences',
-          style: TextStyle(fontSize: 18, fontWeight: FontWeight.bold),
+          style: TextStyle(fontSize: 16, fontWeight: FontWeight.bold),
         ),
         const SizedBox(height: 10),
         ListTile(
@@ -269,9 +250,8 @@ class ProfileScreen extends StatelessWidget {
           title: const Text('Email Notifications'),
           trailing: Switch(
             value: true, 
-            onChanged: (bool value) {
-              
-            },
+            onChanged: (bool value) {},
+            activeColor: Color.fromARGB(255, 169, 121, 121),
           ),
         ),
         ListTile(
@@ -279,9 +259,8 @@ class ProfileScreen extends StatelessWidget {
           title: const Text('SMS Notifications'),
           trailing: Switch(
             value: false, 
-            onChanged: (bool value) {
-              
-            },
+            onChanged: (bool value) {},
+            activeColor: Color.fromARGB(255, 169, 121, 121),
           ),
         ),
       ],
@@ -295,24 +274,20 @@ class ProfileScreen extends StatelessWidget {
       children: [
         const Text(
           'Linked Accounts',
-          style: TextStyle(fontSize: 18, fontWeight: FontWeight.bold),
+          style: TextStyle(fontSize: 16, fontWeight: FontWeight.bold),
         ),
         const SizedBox(height: 10),
         ListTile(
           leading: const Icon(Icons.account_circle),
           title: const Text('Google Account'),
           trailing: const Icon(Icons.check_circle),
-          onTap: () {
-            
-          },
+          onTap: () {},
         ),
         ListTile(
           leading: const Icon(Icons.account_balance_wallet),
           title: const Text('Facebook Account'),
           trailing: const Icon(Icons.check_circle),
-          onTap: () {
-            
-          },
+          onTap: () {},
         ),
       ],
     );
