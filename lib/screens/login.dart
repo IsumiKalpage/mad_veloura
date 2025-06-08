c90e576cb995a6a2d506e8f02028b63155bcc4ec
IsumiKalpage <isumikalpage@gmail.com>
2025-06-09 01:49:52 +0530

    chnaged the style of login and register screens

--- lib/screens/login.dart
+++ lib/screens/login.dart
@@ -1,7 +1,7 @@
 import 'package:flutter/material.dart';
-import 'package:mad_veloura/models/components/bottomnavbar.dart';
+import 'package:mad_veloura/models/components/bottomnavbar.dart'; 
 import 'register.dart';
-import '../models/user/user_list.dart';
+import '../models/user/user_list.dart'; 
 
 class LoginScreen extends StatefulWidget {
   const LoginScreen({super.key});
@@ -19,11 +19,11 @@ class _LoginScreenState extends State<LoginScreen> {
     String email = emailController.text;
     String password = passwordController.text;
 
-    // Check if the entered credentials match any user in the list
+    // Validate user credentials
     bool isValidUser = users.any((user) => user.email == email && user.password == password);
 
     if (isValidUser) {
-      // If valid, navigate to BottomNavbar 
+      // If valid, navigate to BottomNavbar screen
       Navigator.pushReplacement(
         context,
         MaterialPageRoute(builder: (context) => const BottomNavbar()), 
@@ -40,167 +40,194 @@ class _LoginScreenState extends State<LoginScreen> {
   Widget build(BuildContext context) {
     return Scaffold(
       backgroundColor: Colors.white,
-      body: Center(
-        child: SingleChildScrollView(
-          child: Padding(
-            padding: const EdgeInsets.symmetric(horizontal: 20.0),
-            child: Column(
-              mainAxisAlignment: MainAxisAlignment.center,
-              crossAxisAlignment: CrossAxisAlignment.center,
-              children: [
-                
-                Image.asset(
-                  'assets/images/logo.png',
-                  height: 180, 
-                  width: 180,
+      body: SingleChildScrollView( 
+        child: Column(
+          children: [
+            // Logo
+            Container(
+              alignment: Alignment.topCenter,
+              padding: EdgeInsets.only(top: 6),
+              child: Image.asset(
+                'assets/images/logo.png', 
+                height: 180, 
+                width: 180,
+              ),
+            ),
+            
+            
+            Container(
+              width: double.infinity,  
+              decoration: BoxDecoration(
+                color: const Color.fromARGB(255, 255, 241, 246),
+                borderRadius: BorderRadius.only(
+                  topRight: Radius.circular(120),  
                 ),
-                SizedBox(height: 20), 
-
-                
-                Container(
-                  decoration: BoxDecoration(
-                    boxShadow: [
-                      BoxShadow(
-                        color: Colors.grey.withOpacity(0.3),
-                        blurRadius: 10,
-                        spreadRadius: 5,
-                        offset: Offset(0, 4),
+              ),
+              child: Padding(
+                padding: const EdgeInsets.symmetric(horizontal: 20.0, vertical: 40.0),
+                child: Column(
+                  children: [
+                    
+                    Text(
+                      'Login Here',
+                      style: TextStyle(
+                        fontSize: 30,
+                        fontWeight: FontWeight.w500,
+                        color: const Color.fromARGB(255, 64, 12, 28),
                       ),
-                    ],
-                    color: Colors.white,
-                  ),
-                  padding: EdgeInsets.symmetric(horizontal: 20, vertical: 40), 
-                  width: double.infinity,
-                  child: Column(
-                    children: [
-                      
-                      Text(
+                    ),
+                    SizedBox(height: 20),
+
+                    // Email input field
+                    TextField(
+                      controller: emailController,
+                      decoration: InputDecoration(
+                        labelText: 'Email',
+                        prefixIcon: Icon(Icons.email),
+                        border: OutlineInputBorder(),
+                      ),
+                    ),
+                    SizedBox(height: 20),
+
+                    // Password input field
+                    TextField(
+                      controller: passwordController,
+                      obscureText: true,
+                      decoration: InputDecoration(
+                        labelText: 'Password',
+                        prefixIcon: Icon(Icons.lock),
+                        border: OutlineInputBorder(),
+                      ),
+                    ),
+                    SizedBox(height: 20),
+
+                    // Forgot Password link
+                    GestureDetector(
+                      onTap: () {
+                        
+                      },
+                      child: Text(
+                        'Forgot Password?',
+                        style: TextStyle(
+                          color: const Color.fromARGB(255, 15, 15, 15),
+                          fontSize: 16,
+                          decoration: TextDecoration.underline,
+                        ),
+                      ),
+                    ),
+                    SizedBox(height: 30),
+
+                    // Login Button
+                    ElevatedButton(
+                      onPressed: _login,
+                      style: ElevatedButton.styleFrom(
+                        backgroundColor: const Color.fromARGB(255, 239, 191, 183), 
+                        padding: EdgeInsets.symmetric(horizontal: 60, vertical: 16),
+                        shape: RoundedRectangleBorder(
+                          borderRadius: BorderRadius.circular(8),
+                        ),
+                        minimumSize: Size(double.infinity, 55),
+                      ),
+                      child: Text(
                         'Login',
                         style: TextStyle(
-                          color: Colors.black,
-                          fontSize: 32,
-                          fontWeight: FontWeight.w300, 
-                          fontFamily: 'Roboto', 
+                          color: Colors.white,
+                          fontSize: 18,
                         ),
                       ),
-                      SizedBox(height: 30), 
-                      // Email input field with icon
-                      Container(
-                        decoration: BoxDecoration(
-                          color: Colors.grey[100],
-                        ),
-                        child: TextField(
-                          controller: emailController,
-                          decoration: InputDecoration(
-                            prefixIcon: Icon(Icons.email),
-                            hintText: 'Enter your email',
-                            border: InputBorder.none,
-                            contentPadding: EdgeInsets.symmetric(vertical: 15.0, horizontal: 20.0),
-                          ),
-                        ),
-                      ),
-                      SizedBox(height: 20), 
-
-                      // Password input field with icon
-                      Container(
-                        decoration: BoxDecoration(
-                          color: Colors.grey[100],
-                        ),
-                        child: TextField(
-                          controller: passwordController,
-                          obscureText: true,
-                          decoration: InputDecoration(
-                            prefixIcon: Icon(Icons.lock),
-                            hintText: 'Enter your password',
-                            border: InputBorder.none,
-                            contentPadding: EdgeInsets.symmetric(vertical: 15.0, horizontal: 20.0),
-                          ),
-                        ),
-                      ),
-                      SizedBox(height: 20), 
-                      // Forgot Password link
-                      GestureDetector(
-                        onTap: () {
-                          
-                        },
-                        child: Text(
-                          "Forgot Password?",
-                          style: TextStyle(
-                            color: const Color.fromARGB(255, 164, 161, 162),
-                            fontSize: 16,
-                            decoration: TextDecoration.underline,
-                          ),
-                        ),
-                      ),
-                      SizedBox(height: 30),
-
-                      // Login Button
-                      ElevatedButton(
-                        onPressed: _login,
-                        style: ElevatedButton.styleFrom(
-                          backgroundColor: Color(0xFF823434), 
-                          padding: EdgeInsets.symmetric(horizontal: 60, vertical: 16),
-                          shape: RoundedRectangleBorder(
-                            borderRadius: BorderRadius.circular(0), 
-                          ),
-                          elevation: 8,
-                          minimumSize: Size(double.infinity, 55),
-                        ),
-                        child: Text(
-                          'Login',
-                          style: TextStyle(
-                            color: Colors.white,
-                            fontSize: 18,
-                          ),
-                        ),
-                      ),
-                    ],
-                  ),
-                ),
-                SizedBox(height: 20),
-                GestureDetector(
-                  onTap: () {
-                    // Navigate to Sign Up screen
-                    Navigator.push(
-                      context,
-                      MaterialPageRoute(builder: (context) => const RegisterScreen()),
-                    );
-                  },
-                  child: Text(
-                    "Don't have an account? Sign up",
-                    style: TextStyle(
-                      color: Colors.black,
-                      fontSize: 16,
-                      decoration: TextDecoration.underline,
-                    ),
-                  ),
-                ),
-                SizedBox(height: 30),
-
-                // Social Media Login 
-                Row(
-                  mainAxisAlignment: MainAxisAlignment.center,
-                  children: [
-                    IconButton(
-                      icon: Icon(Icons.g_translate),
-                      onPressed: () {},
-                      color: const Color.fromARGB(255, 113, 203, 116),
-                    ),
-                    IconButton(
-                      icon: Icon(Icons.facebook),
-                      onPressed: () {},
-                      color: const Color.fromARGB(255, 6, 65, 192),
-                    ),
-                    IconButton(
-                      icon: Icon(Icons.camera_alt), // Instagram icon
-                      onPressed: () {},
-                      color: const Color.fromARGB(255, 213, 68, 117),
                     ),
                   ],
                 ),
+              ),
+            ),
+            SizedBox(height: 20),
+            GestureDetector(
+              onTap: () {
+                Navigator.push(
+                  context,
+                  MaterialPageRoute(builder: (context) => const RegisterScreen()),
+                );
+              },
+              child: Text(
+                "Don't have an account? Sign up",
+                style: TextStyle(
+                  color: Colors.black,
+                  fontSize: 16,
+                  decoration: TextDecoration.underline,
+                ),
+              ),
+            ),
+            SizedBox(height: 30),
+
+            Row(
+              mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+              children: [
+                // Google 
+                Container(
+                  width: 60,
+                  height: 60,
+                  decoration: BoxDecoration(
+                    border: Border.all(color: Colors.grey),
+                    shape: BoxShape.circle,
+                  ),
+                  child: IconButton(
+                    icon: Image.asset('assets/images/google.png'), 
+                    onPressed: () {
+                      // Handle Google login
+                    },
+                  ),
+                ),
+                
+                // Facebook
+                Container(
+                  width: 60,
+                  height: 60,
+                  decoration: BoxDecoration(
+                    border: Border.all(color: Colors.grey),
+                    shape: BoxShape.circle,
+                  ),
+                  child: IconButton(
+                    icon: Image.asset('assets/images/facebook.png'), 
+                    onPressed: () {
+                     
+                    },
+                  ),
+                ),
+                
+                // Twitter
+                Container(
+                  width: 60,
+                  height: 60,
+                  decoration: BoxDecoration(
+                    border: Border.all(color: Colors.grey),
+                    shape: BoxShape.circle,
+                  ),
+                  child: IconButton(
+                    icon: Image.asset('assets/images/twitter.png'), 
+                    onPressed: () {
+                    },
+                  ),
+                ),
+                
+                // Instagram
+                Container(
+                  width: 60,
+                  height: 60,
+                  decoration: BoxDecoration(
+                    border: Border.all(color: Colors.grey),
+                    shape: BoxShape.circle,
+                  ),
+                  child: IconButton(
+                    icon: Image.asset('assets/images/insta.png'), 
+                    onPressed: () {
+                      
+                    },
+                  ),
+                ),
               ],
             ),
-          ),
+            SizedBox(height: 20),
+          ],
         ),
       ),
     );
--- lib/screens/register.dart
+++ lib/screens/register.dart
@@ -7,127 +7,122 @@ class RegisterScreen extends StatelessWidget {
   Widget build(BuildContext context) {
     return Scaffold(
       backgroundColor: Colors.white,
-      appBar: AppBar(
-        leading: IconButton(
-          icon: Icon(Icons.arrow_back),
-          onPressed: () {
-            Navigator.pop(context);  // Navigate back to the login screen
-          },
-        ),
-        backgroundColor: Color.fromARGB(255, 255, 255, 255),
-      ),
-      body: Center(
-        child: SingleChildScrollView(
-          child: Padding(
-            padding: const EdgeInsets.symmetric(horizontal: 20.0),
-            child: Column(
-              mainAxisAlignment: MainAxisAlignment.center,
-              children: [
-                // Logo image
-                Image.asset(
-                  'assets/images/logo.png',
-                  height: 150,
-                  width: 150,
-                ),
-                
-                // Register Text
-                SizedBox(height: 20),
-                Text(
-                  'Register',
-                  style: TextStyle(
-                    color: Colors.black,
-                    fontSize: 32,
-                    fontWeight: FontWeight.w600,
-                  ),
-                ),
-
-                // Name input field
-                SizedBox(height: 30),
-                TextField(
-                  decoration: InputDecoration(
-                    labelText: 'Full Name',
-                    labelStyle: TextStyle(color: Colors.black),
-                    filled: true,
-                    fillColor: Colors.grey[200],
-                    border: OutlineInputBorder(
-                      borderRadius: BorderRadius.circular(12.0),
-                    ),
-                    contentPadding: EdgeInsets.symmetric(vertical: 15.0, horizontal: 20.0),
-                  ),
-                ),
-
-                // Email input field
-                SizedBox(height: 20),
-                TextField(
-                  decoration: InputDecoration(
-                    labelText: 'Email',
-                    labelStyle: TextStyle(color: Colors.black),
-                    filled: true,
-                    fillColor: Colors.grey[200],
-                    border: OutlineInputBorder(
-                      borderRadius: BorderRadius.circular(12.0),
-                    ),
-                    contentPadding: EdgeInsets.symmetric(vertical: 15.0, horizontal: 20.0),
-                  ),
-                ),
-
-                // Phone number input field
-                SizedBox(height: 20),
-                TextField(
-                  keyboardType: TextInputType.phone,
-                  decoration: InputDecoration(
-                    labelText: 'Phone Number',
-                    labelStyle: TextStyle(color: Colors.black),
-                    filled: true,
-                    fillColor: Colors.grey[200],
-                    border: OutlineInputBorder(
-                      borderRadius: BorderRadius.circular(12.0),
-                    ),
-                    contentPadding: EdgeInsets.symmetric(vertical: 15.0, horizontal: 20.0),
-                  ),
-                ),
-
-                // Password input field
-                SizedBox(height: 20),
-                TextField(
-                  obscureText: true,
-                  decoration: InputDecoration(
-                    labelText: 'Password',
-                    labelStyle: TextStyle(color: Colors.black),
-                    filled: true,
-                    fillColor: Colors.grey[200],
-                    border: OutlineInputBorder(
-                      borderRadius: BorderRadius.circular(12.0),
-                    ),
-                    contentPadding: EdgeInsets.symmetric(vertical: 15.0, horizontal: 20.0),
-                  ),
-                ),
-
-                // Register button
-                SizedBox(height: 30),
-                ElevatedButton(
-                  onPressed: () {
-                    // Handle registration action here
-                  },
-                  style: ElevatedButton.styleFrom(
-                    backgroundColor: Color(0xFF823434), 
-                    padding: EdgeInsets.symmetric(horizontal: 80, vertical: 16),
-                    shape: RoundedRectangleBorder(
-                      borderRadius: BorderRadius.circular(12),
-                    ),
-                    elevation: 5,
-                  ),
-                  child: Text(
-                    'Register',
-                    style: TextStyle(
-                      color: Colors.white,
-                      fontSize: 18,
-                    ),
-                  ),
-                ),
-              ],
+      body: SingleChildScrollView( 
+        child: Column(
+          children: [
+            
+            Container(
+              alignment: Alignment.topCenter,
+              padding: EdgeInsets.only(top: 6),
+              child: Image.asset(
+                'assets/images/logo.png',
+                height: 180,
+                width: 180,
+              ),
             ),
-          ),
+            SizedBox(height: 20),
+
+           
+            Container(
+              width: double.infinity,
+              decoration: BoxDecoration(
+                color: const Color.fromARGB(255, 255, 241, 246),
+                borderRadius: BorderRadius.only(
+                  topLeft: Radius.circular(120), 
+                ),
+              ),
+              child: Padding(
+                padding: const EdgeInsets.symmetric(horizontal: 20.0, vertical: 40.0),
+                child: Column(
+                  children: [
+                    
+                    Text(
+                      'Register',
+                      style: TextStyle(
+                        color: const Color.fromARGB(255, 64, 12, 28),
+                        fontSize: 32,
+                        fontWeight: FontWeight.w300, 
+                      ),
+                    ),
+                    SizedBox(height: 30),
+
+                    // Full Name input field
+                    TextField(
+                      decoration: InputDecoration(
+                        labelText: 'Full Name',
+                        labelStyle: TextStyle(color: Colors.black),
+                        prefixIcon: Icon(Icons.person),
+                        border: OutlineInputBorder(),
+                        contentPadding: EdgeInsets.symmetric(vertical: 15.0, horizontal: 20.0),
+                      ),
+                    ),
+                    SizedBox(height: 20),
+
+                    // Email input field
+                    TextField(
+                      decoration: InputDecoration(
+                        labelText: 'Email',
+                        labelStyle: TextStyle(color: Colors.black),
+                        prefixIcon: Icon(Icons.email),
+                        border: OutlineInputBorder(),
+                        contentPadding: EdgeInsets.symmetric(vertical: 15.0, horizontal: 20.0),
+                      ),
+                    ),
+                    SizedBox(height: 20),
+
+                    // Phone number input field
+                    TextField(
+                      keyboardType: TextInputType.phone,
+                      decoration: InputDecoration(
+                        labelText: 'Phone Number',
+                        labelStyle: TextStyle(color: Colors.black),
+                        prefixIcon: Icon(Icons.phone),
+                        border: OutlineInputBorder(),
+                        contentPadding: EdgeInsets.symmetric(vertical: 15.0, horizontal: 20.0),
+                      ),
+                    ),
+                    SizedBox(height: 20),
+
+                    // Password input field
+                    TextField(
+                      obscureText: true,
+                      decoration: InputDecoration(
+                        labelText: 'Password',
+                        labelStyle: TextStyle(color: Colors.black),
+                        prefixIcon: Icon(Icons.lock),
+                        border: OutlineInputBorder(),
+                        contentPadding: EdgeInsets.symmetric(vertical: 15.0, horizontal: 20.0),
+                      ),
+                    ),
+                    SizedBox(height: 30),
+
+                    // Register button
+                    ElevatedButton(
+                      onPressed: () {
+                        
+                      },
+                      style: ElevatedButton.styleFrom(
+                        backgroundColor: const Color.fromARGB(255, 239, 191, 183), 
+                        padding: EdgeInsets.symmetric(horizontal: 60, vertical: 16),
+                        shape: RoundedRectangleBorder(
+                          borderRadius: BorderRadius.circular(8),
+                        ),
+                        elevation: 8,
+                        minimumSize: Size(double.infinity, 55),
+                      ),
+                      child: Text(
+                        'Register',
+                        style: TextStyle(
+                          color: Colors.white,
+                          fontSize: 18,
+                        ),
+                      ),
+                    ),
+                  ],
+                ),
+              ),
+            ),
+          ],
         ),
       ),
     );
