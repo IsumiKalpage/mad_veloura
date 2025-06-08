a5d61eba828f30ff3db527ab7e12c296f35c6d5f
IsumiKalpage <isumikalpage@gmail.com>
2025-06-08 12:59:47 +0530

    added the skincare products into the products page

--- lib/screens/products.dart
+++ lib/screens/products.dart
@@ -10,11 +10,9 @@ class ProductsScreen extends StatelessWidget {
       appBar: TopNavbar(),
       body: Container(
         color: Colors.white,
-        width: double.infinity,
-        height: double.infinity,
         padding: const EdgeInsets.all(16.0),
         child: Column(
-          crossAxisAlignment: CrossAxisAlignment.center,
+          crossAxisAlignment: CrossAxisAlignment.start,
           children: [
             Text(
               'Products',
@@ -36,8 +34,152 @@ class ProductsScreen extends StatelessWidget {
               ),
             ),
             const SizedBox(height: 16),
+            Expanded(
+              child: GridView.builder(
+                shrinkWrap: true,
+                physics: BouncingScrollPhysics(),
+                gridDelegate: SliverGridDelegateWithFixedCrossAxisCount(
+                  crossAxisCount: 2,
+                  crossAxisSpacing: 16.0,
+                  mainAxisSpacing: 16.0,
+                  childAspectRatio: 0.6,
+                ),
+                itemCount: 4,
+                itemBuilder: (context, index) {
+                  return ProductCard(
+                    imageUrl: _getImageUrl(index),
+                    name: _getProductName(index),
+                    price: _getProductPrice(index),
+                    rating: _getProductRating(index),
+                  );
+                },
+              ),
+            ),
           ],
         ),
+      ),
+    );
+  }
+
+  String _getImageUrl(int index) {
+    List<String> imageUrls = [
+      'assets/images/aha.webp',
+      'assets/images/anuaa.webp',
+      'assets/images/anuac.webp',
+      'assets/images/anuaF.webp',
+    ];
+    return imageUrls[index];
+  }
+
+  String _getProductName(int index) {
+    List<String> productNames = [
+      'The Ordinary AHA 30% + BHA 2% Peeling Solution',
+      'ANUA Niacinamide 10% + TXA 4% Dark Spot Correcting Serum 30ml',
+      'ANUA Heartleaf Pore Control Cleansing Oil 200ml',
+      'ANUA Heartleaf Quercetinol Pore Deep Cleansing Foam 150ml',
+    ];
+    return productNames[index];
+  }
+
+  String _getProductPrice(int index) {
+    List<String> productPrices = [
+      'LKR 3,500', 
+      'LKR 5,500', 
+      'LKR 7,500',
+      'LKR 8,500', 
+    ];
+    return productPrices[index];
+  }
+
+  double _getProductRating(int index) {
+    List<double> productRatings = [
+      4.0,
+      4.5,
+      3.5,
+      5.0,
+    ];
+    return productRatings[index];
+  }
+}
+
+class ProductCard extends StatelessWidget {
+  final String imageUrl;
+  final String name;
+  final String price;
+  final double rating;
+
+  const ProductCard({
+    required this.imageUrl,
+    required this.name,
+    required this.price,
+    required this.rating,
+  });
+
+  @override
+  Widget build(BuildContext context) {
+    return Card(
+      elevation: 4.0,
+      shape: RoundedRectangleBorder(
+        borderRadius: BorderRadius.circular(12.0),
+      ),
+      child: Column(
+        children: [
+          Image.asset(
+            imageUrl,
+            width: double.infinity, 
+            height: 200, 
+            fit: BoxFit.cover,
+          ),
+          Padding(
+            padding: const EdgeInsets.all(12.0),
+            child: Column(
+              crossAxisAlignment: CrossAxisAlignment.start,
+              children: [
+                Text(
+                  name,
+                  style: TextStyle(
+                    fontSize: 14,
+                    fontWeight: FontWeight.w200,
+                    color: Colors.black,
+                  ),
+                  maxLines: 2,
+                  overflow: TextOverflow.ellipsis,
+                ),
+                SizedBox(height: 8.5),
+                Text(
+                  price,
+                  style: TextStyle(
+                    fontSize: 12,
+                    fontWeight: FontWeight.w400,
+                    color: Colors.grey,
+                  ),
+                ),
+                SizedBox(height: 9.0),
+                Row(
+                  children: List.generate(
+                    5,
+                    (index) => Icon(
+                      index < rating ? Icons.star : Icons.star_border,
+                      color: Colors.amber,
+                      size: 17.0,
+                    ),
+                  ),
+                ),
+                SizedBox(height: 8.0),
+                ElevatedButton(
+                  onPressed: () {},
+                  child: Text('Add to Cart'),
+                  style: ElevatedButton.styleFrom(
+                    foregroundColor: Colors.white,
+                    backgroundColor: Color.fromARGB(255, 169, 121, 121),
+                    minimumSize: Size(double.infinity, 36),
+                    padding: EdgeInsets.symmetric(vertical: 8.0),
+                  ),
+                ),
+              ],
+            ),
+          ),
+        ],
       ),
     );
   }
